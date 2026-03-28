1a44136adc929b142bd2c90aa2a8dcc4aeef7442
DCComplex <lahiruchamika1212@gmail.com>
2026-03-29 01:04:16 +0530

    feat: update workflow

---
+++ tests/Pest.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Pest Configuration
+|--------------------------------------------------------------------------
+| This file is loaded automatically by Pest. Here you can configure
+| which tests use which base classes, and set global helper functions.
+*/
+
+uses()->in('tests');
